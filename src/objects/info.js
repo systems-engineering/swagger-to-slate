798877a609ea320d4f8721470398d0d4ee3e7463
Emil Kampp <40206+ekampp@users.noreply.github.com>
2021-02-28 21:40:50 +0100

    Update info.js

--- src/objects/info.js
+++ src/objects/info.js
@@ -15,13 +15,7 @@ function parse(info){
 
         // Set the language tab
         res.push(`language_tabs: \n   - shell \n`)
-
-        // Set the footer
-        res.push(`toc_footers: \n   - <a href='#'>Sign Up for a Developer Key</a> \n   - <a href='https://github.com/lavkumarv'>Documentation Powered by lav</a> \n`)
         
-        // Include the error codes
-        res.push(`includes: \n   - errors \n`)
-
         // Include the search options
         res.push(`search: true \n`)
 
